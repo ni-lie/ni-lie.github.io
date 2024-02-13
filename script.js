2a4eb8c47eb420c754bcfa3124c3227851cc090c
ni-lie <cjay.coronel25@gmail.com>
2024-02-13 20:22:04 +0800

    feat: make it mobile responsive

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function opentab(tabname, event) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add('active-tab');
 }
+
+var sidemenu = document.getElementById("sidemenu")
+
+function openMenu() {
+    sidemenu.style.right = "0";
+}
+
+function closeMenu() {
+    sidemenu.style.right = "-200px";
+}
