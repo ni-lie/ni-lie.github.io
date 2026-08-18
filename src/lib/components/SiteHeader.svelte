2c89958015bad48e9b43decc0669e52ed4367bdb
ni-lie <williejr@twelvebaskets.com.ph>
2026-08-18 14:58:55 +0800

    chore: remove experience and contact from the header

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -2,10 +2,8 @@
   let menuOpen = false;
 
   const links = [
-    { href: '/#experience', label: 'Experience' },
     { href: '/projects/', label: 'Projects' },
     { href: '/blog/', label: 'Blog' },
-    { href: '/#contact', label: 'Contact' }
   ];
 </script>
 
